0bc65669c48983dced7ddc8ac4a5b83c7029e5b2
tnehil <tom.nehil@gmail.com>
2022-12-08 17:01:37 -0600

    Documentin

--- data-processing/deploy-to-lambda.sh
+++ data-processing/deploy-to-lambda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd updateFatalEncounters/.venv/lib/python3.9/site-packages
+# Only include dependencies for pyairtable as boto3 is already installed on Lambda
 zip -r ../../../../../lambda-deploy.zip certifi charset_normalizer idna pyairtable requests
 cd ../../../../
 zip -g ../lambda-deploy.zip lambda_function.py
