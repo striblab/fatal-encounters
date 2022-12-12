7727f2827a9a3bbda769d81f9fca3e9e0125beff
tnehil <tom.nehil@gmail.com>
2022-12-12 14:20:40 -0600

    Fixes local/deployed webfonts issues

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import App from './App.svelte'
 if (import.meta.env.DEV) {
   import("./scss/local-webfonts.scss")
 } 
-if (import.meta.env.PROD) {
-  import("./scss/strib-webfonts.scss")
-}
 
 const app = new App({
   target: document.getElementById('proj-container')
